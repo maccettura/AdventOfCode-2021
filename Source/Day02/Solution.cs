97f1f7519f4016738ceb31db40889e6d71ae4478
Michelangelo Accettura <MAccettura@rightpoint.com>
2021-12-02 16:14:24 -0500

    Adding day 2

--- Source/Day02/Solution.cs
+++ Source/Day02/Solution.cs
@@ -1,20 +1,61 @@
 ﻿using System;
+using System.Linq;
 
 namespace AdventOfCode.Day02;
 
 public class Solution : BaseSolution
 {
-    public Solution() : base(2, "")
+    public Solution() : base(2, "Dive!")
     {
     }
 
     public override string GetPart1Answer()
     {
-        return string.Empty;
+        int position = 0;
+        int depth = 0;
+
+        foreach(var input in GetResourceString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            int mod = int.Parse(new string(input.Where(c => char.IsDigit(c)).ToArray()));
+            if(input[0] == 'f')
+            {
+                position += mod;
+            }
+            else if(input[0] == 'd')
+            {
+                depth += mod;
+            }
+            else if(input[0] == 'u')
+            {
+                depth -= mod;
+            }
+        }
+        return (position * depth).ToString();
     }
 
     public override string GetPart2Answer()
     {
-        return string.Empty;
+        int position = 0;
+        int depth = 0;
+        int aim = 0;
+
+        foreach (var input in GetResourceString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+        {
+            int mod = int.Parse(new string(input.Where(c => char.IsDigit(c)).ToArray()));
+            if (input[0] == 'f')
+            {
+                position += mod;
+                depth += aim * mod;
+            }
+            else if (input[0] == 'd')
+            {
+                aim += mod;
+            }
+            else if (input[0] == 'u')
+            {
+                aim -= mod;
+            }
+        }
+        return (position * depth).ToString();
     }
 }
